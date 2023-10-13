589e2ddf392e421ac20a17c7731d1153f2b0a861
Your Name <emawebsolutions.g@gmail.com>
2023-10-13 13:36:58 +0100

    gooooo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -43,7 +43,7 @@ class AppServiceProvider extends ServiceProvider
                 //
             }
 
-           $all_menus = Menu::where('is_static', INACTIVE)->with('submenus')->latest()->get();
+          $all_menus = Menu::where('is_static', INACTIVE)->with('submenus')->latest()->get();
             $allsettings = allsetting();
             view()->share(['all_menus' => $all_menus, 'allsettings' => $allsettings]);
         
