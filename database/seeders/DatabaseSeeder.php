01c3a1243433ebfd6be2783bdc45ecdb97650fa7
Your Name <emawebsolutions.g@gmail.com>
2023-10-13 12:27:34 +0100

    gooood

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(LanguageTableSeeder::class);
-        $this->call(UserTableSeeder::class);
+     //   $this->call(UserTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(SliderTableSeeder::class);
         $this->call(AdvertiseTableSeeder::class);
