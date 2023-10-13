037ed81e980b3556d62a1a2d201a2caf7d350461
Your Name <emawebsolutions.g@gmail.com>
2023-10-13 11:49:05 +0100

    yooo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use App\Models\Menu;
 use App\Models\Setting;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -43,9 +44,15 @@ class AppServiceProvider extends ServiceProvider
                 //
             }
 
-            $all_menus = Menu::where('is_static', INACTIVE)->with('submenus')->latest()->get();
-            $allsettings = allsetting();
-            view()->share(['all_menus' => $all_menus, 'allsettings' => $allsettings]);
+       
+
+$tableName = 'menus'; // Replace with the actual table name
+
+if (DB::connection()->getSchemaBuilder()->hasTable($tableName)) {
+    $all_menus = Menu::where('is_static', INACTIVE)->with('submenus')->latest()->get();
+    $allsettings = allsetting();
+    view()->share(['all_menus' => $all_menus, 'allsettings' => $allsettings]);
+}
         
     }
 }
